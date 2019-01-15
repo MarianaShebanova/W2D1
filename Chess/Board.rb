1f78eed960c56e6d615b10540dc47cf5d30c2171
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-01-14 16:54:07 -0800

    Initial commit for Piece

--- Chess/Board.rb
+++ Chess/Board.rb
@@ -1,3 +1,41 @@
 class Board
-    
+    attr_reader :rows, :sentinel, :grid
+
+    def initialize
+        @rows = Array.new(8) { Array.new(8) }
+    end
+
+    def [](pos)
+    end
+
+    def []=(pos, val)
+    end
+
+    def move_piece(color, start_pos, end_pos)
+    end
+
+    def valid_pos?(pos)
+    end
+
+    def add_piece(piece, pos)
+    end
+
+    def checkmate?(color)
+    end
+
+    def in_check?(color)
+    end
+
+    def find_king(color)
+    end
+
+    def pieces
+    end
+
+    def dup
+    end
+
+    def move_piece!(color, start_pos, end_pos)
+    end
+
 end
